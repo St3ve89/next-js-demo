80836ca9e38a3ad304913504b791b43d65abd2ff
St3ve89 <istvan.acs89@gmail.com>
2021-05-11 15:46:58 +0100

    Using the api data

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
+import { server } from '../../../config';
 
 const article = ({ article }) => {
   // const router = useRouter();
@@ -17,9 +18,7 @@ const article = ({ article }) => {
 };
 
 export const getStaticProps = async (context) => {
-  const res = await fetch(
-    `https://jsonplaceholder.typicode.com/posts/${context.params.id}`
-  );
+  const res = await fetch(`${server}/api/articles/${context.params.id}`);
 
   const article = await res.json();
 
@@ -31,7 +30,7 @@ export const getStaticProps = async (context) => {
 };
 
 export const getStaticPaths = async () => {
-  const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+  const res = await fetch(`${server}/api/articles`);
 
   const articles = await res.json();
   const ids = articles.map((article) => article.id);
@@ -42,5 +41,32 @@ export const getStaticPaths = async () => {
     fallback: false, // if we go to something which is doesnt exist in the data it will get us a 404 page
   };
 };
+
+// export const getStaticProps = async (context) => {
+//   const res = await fetch(
+//     `https://jsonplaceholder.typicode.com/posts/${context.params.id}`
+//   );
+
+//   const article = await res.json();
+
+//   return {
+//     props: {
+//       article,
+//     },
+//   };
+// };
+
+// export const getStaticPaths = async () => {
+//   const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+
+//   const articles = await res.json();
+//   const ids = articles.map((article) => article.id);
+//   const paths = ids.map((id) => ({ params: { id: id.toString() } }));
+
+//   return {
+//     paths,
+//     fallback: false, // if we go to something which is doesnt exist in the data it will get us a 404 page
+//   };
+// };
 
 export default article;

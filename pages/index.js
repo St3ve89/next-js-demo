ceead3e824de0a6a61090192e32f5cfb1487393d
St3ve89 <istvan.acs89@gmail.com>
2021-05-11 12:16:43 +0100

    Create layout component

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
@@ -9,7 +8,7 @@ export default function Home() {
         <title>WebDev Newz</title>
         <meta name="keywords" content="web development, programming" />
       </Head>
-      <h1>welcome to next</h1>
+      <h1>Welcome to next</h1>
     </div>
   );
 }
